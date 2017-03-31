deacf7570f27176a80115bb364bc5a4af5136ce4
Antony Woods <antony@mastodonc.com>
2017-03-31 14:22:46 +0100

    Add promote.sh

---
+++ promote.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set +x
+echo -n "Container ID (minus tag, e.g. mastodonc/foobar) "
+read id
+
+ID=${id:?Error: You must provide a container ID}
+
+echo -n "Container Tag (e.g. latest, git-408aed) "
+read tag
+
+TAG=${tag:?Error: You must provide a container tag}
+FULL_TAG=$(echo $ID:$TAG)
+
+echo "Pulling $FULL_TAG"
+docker pull $FULL_TAG
+IMG_ID=$(docker images | grep -m1 -E "$ID\s+$TAG" | awk '{print $3}')
+RELEASE_TIME=$(date +%Y-%m-%dT%H-%M-%S)
+NEW_TAG=$(echo $FULL_TAG"-release-"$RELEASE_TIME)
+
+echo "Tagging $IMG_ID as $NEW_TAG"
+echo docker tag $IMG_ID $NEW_TAG
+docker tag $IMG_ID $NEW_TAG
+
+echo "Pushing $NEW_TAG"
+docker push $NEW_TAG
