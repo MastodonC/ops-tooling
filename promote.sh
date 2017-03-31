b4ea803c667fb01714c3dcc26f1fcc4053c0706e
Antony Woods <antony@mastodonc.com>
2017-03-31 15:23:02 +0100

    Remove logging

--- promote.sh
+++ promote.sh
@@ -7,7 +7,6 @@ ID=${1:?Error: You must provide a container ID}
 TAG=${2:?Error: You must provide a container tag}
 FULL_TAG=$(echo $ID:$TAG)
 
-echo "Pulling $FULL_TAG"
 docker pull $FULL_TAG
 
 if [ $? -eq 0 ]
@@ -15,14 +14,8 @@ then
     IMG_ID=$(docker images | grep -m1 -E "$ID\s+$TAG" | awk '{print $3}')
     RELEASE_TIME=$(date +%Y-%m-%dT%H-%M-%S)
     NEW_TAG=$(echo $FULL_TAG"-release-"$RELEASE_TIME)
-
-    echo "Tagging $IMG_ID as $NEW_TAG"
-    echo docker tag $IMG_ID $NEW_TAG
     docker tag $IMG_ID $NEW_TAG
-
-    echo "Pushing $NEW_TAG"
     docker push $NEW_TAG
-
     echo $NEW_TAG
 else
     exit 1
